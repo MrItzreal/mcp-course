6d119bc2a7f6590c4b462cf3c00e2702aa4bd1c3
MrItzreal <is.wsie@gmail.com>
2025-07-30 16:47:49 -0400

    feat: add functionality to create random users with fake data and update user using: Sampling.

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 import fs from "node:fs/promises";
+import { CreateMessageResultSchema } from "@modelcontextprotocol/sdk/types.js";
 
 // Creates an MCP server
 const server = new McpServer({
@@ -102,6 +103,64 @@ server.tool(
   async (params) => {
     try {
       const id = await createUser(params);
+      return {
+        content: [{ type: "text", text: `User ${id} created successfully` }],
+      };
+    } catch {
+      return {
+        content: [{ type: "text", text: "Failed to save user" }],
+      };
+    }
+  }
+);
+
+// Sampling config
+server.tool(
+  "create-random-user",
+  "Create a random user with fake data",
+  {
+    title: "Create Random User",
+    readOnlyHint: false,
+    destructiveHint: false,
+    idempotentHint: false,
+    openWorldHint: true,
+  },
+  async () => {
+    const res = await server.server.request(
+      {
+        method: "sampling/createMessage",
+        params: {
+          messages: [
+            {
+              role: "user",
+              content: {
+                type: "text",
+                text: "Generate fake user data. The user should have a realistic name, email, address, and phone number. Return this data as a JSON object with no other text or formatter so it can be used with JSON.parse.",
+              },
+            },
+          ],
+          maxTokens: 1024,
+        },
+      },
+      CreateMessageResultSchema
+    );
+
+    if (res.content.type !== "text") {
+      return {
+        content: [{ type: "text", text: "Failed to generate user data" }],
+      };
+    }
+
+    try {
+      const fakeUser = JSON.parse(
+        res.content.text
+          .trim()
+          .replace(/^```json/, "")
+          .replace(/```$/, "")
+          .trim()
+      );
+
+      const id = await createUser(fakeUser);
       return {
         content: [{ type: "text", text: `User ${id} created successfully` }],
       };
